9750bf1b473b43f1ef41e14403472bdedc2d99a8
Valentin Conseil <conseil.valentin96@gmail.com>
2018-04-22 01:04:59 +0200

    auth dropbox & googledrive fonctionnelle

--- DroovyClient/src/app/auth/auth.component.ts
+++ DroovyClient/src/app/auth/auth.component.ts
@@ -17,7 +17,14 @@ export class AuthComponent implements OnInit {
   }
 
   authGoogleDrive(){
+
     window.location.href= "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.metadata.readonly&redirect_uri=http://localhost:8080/droovy/googledriveauth/callback&client_id=783584831345-rpngg6uic1i0iorvp2l5agc9ajmdm64v.apps.googleusercontent.com";
+
+  //  window.location.href= "https://accounts.google.com/o/oauth2/v2/auth?response_type=token&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.metadata.readonly&client_id=783584831345-rpngg6uic1i0iorvp2l5agc9ajmdm64v.apps.googleusercontent.com&access_type=online&redirect_uri=http://localhost:8080/droovy/googledriveauth/callback/";
+  }
+
+  authDropBox(){
+    window.location.href= "https://www.dropbox.com/oauth2/authorize?response_type=code&client_id=i90y72ofs47u9b8&redirect_uri=http://localhost:8080/droovy/dropboxauth/callback";
   }
 
 }
