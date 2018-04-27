b126b7f0e3da9daa18df42b51364e4a21e7d5be1
Valentin Conseil <conseil.valentin96@gmail.com>
2018-04-27 10:56:46 +0200

    Call dropbox

--- src/main/java/com/droovy/request/UserApiRequest.java
+++ src/main/java/com/droovy/request/UserApiRequest.java
@@ -1,10 +1,23 @@
 package com.droovy.request;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+@Path("request")
 public class UserApiRequest {
 
 	
 	
-	
+	@GET
+	@Produces("text/plain")
+	@Path("/list")
+	public String getFilesList(@Context UriInfo uriInfo,@QueryParam("password") String mdp,@QueryParam("id") String id) {
+		return new UserRequestDropbox().getFilesList();
+	}
 	
 	
 	
--- src/main/java/com/droovy/request/UserRequestDropbox.java
+++ src/main/java/com/droovy/request/UserRequestDropbox.java
@@ -1,11 +1,53 @@
 package com.droovy.request;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.JerseyClient;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.glassfish.jersey.client.JerseyInvocation;
+import org.glassfish.jersey.client.JerseyWebTarget;
+
+import com.droovy.DatabaseOp;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
 public class UserRequestDropbox implements UserRequest {
 
 	@Override
 	public String getFilesList() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		DatabaseOp.updateUserDropBoxToken("senU5G790IAAAAAAAAAArfc5TEtCNrjYE6dR_AhHa7MVnVub9BgzDlHb0gmwNHFY");
+		
+		String url = "https://api.dropboxapi.com/2/file_requests/list";
+
+		JerseyClient jerseyClient = JerseyClientBuilder.createClient();
+		JerseyWebTarget jerseyTarget = jerseyClient.target(url);
+		
+		MultivaluedMap<String, String> formData = new MultivaluedHashMap<String, String>();
+		Response response = jerseyTarget.request().header("Authorization", "Bearer "+DatabaseOp.getUserDropBoxToken()).accept(MediaType.APPLICATION_JSON).post(Entity.form(formData));
+		
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus()+ " "+ response.toString());
+		}		JerseyInvocation.Builder jerseyInvocation = jerseyTarget.request("application/json");
+
+		String output =  response.readEntity(String.class);
+	
+		
+		System.out.println("Files from Server .... "+output+"\n");
+		System.out.println(response.toString());
+		
+
+		return "Response : "+output;
+		
+		
+		
+		
 	}
 
 }
