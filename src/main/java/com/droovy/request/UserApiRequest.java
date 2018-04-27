dc5db974be622c1e4231818f0ff9bf6ee6f206d6
valentin <valentin.lion@laposte.net>
2018-04-27 10:56:53 +0200

    Commit

--- src/main/java/com/droovy/request/UserApiRequest.java
+++ src/main/java/com/droovy/request/UserApiRequest.java
@@ -1,8 +1,25 @@
 package com.droovy.request;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+@Path("request")
 public class UserApiRequest {
 
-	
+	@GET
+	@Produces("text/plain")
+	@Path("/googledrive")
+	public String getFileGoogleDrive(@Context UriInfo uriInfo,@QueryParam("code") String code){
+		System.out.println("recu");
+		UserRequest requestGoogleDrive = new UserRequestGoogleDrive();
+		requestGoogleDrive.getFilesList();
+		return "";
+		
+	}
 	
 	
 	
--- src/main/java/com/droovy/request/UserRequestGoogleDrive.java
+++ src/main/java/com/droovy/request/UserRequestGoogleDrive.java
@@ -1,13 +1,41 @@
 package com.droovy.request;
 
-import com.droovy.auth.GoogledriveAuth;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.JerseyClient;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.glassfish.jersey.client.JerseyWebTarget;
+
+import com.droovy.DatabaseOp;
+
 
 public class UserRequestGoogleDrive implements UserRequest {
 
 	@Override
 	public String getFilesList() {
-		
-		return null;
+		String token = DatabaseOp.getUserGoogleDriveToken();
+			
+		String url = "https://www.googleapis.com/drive/v2/files";
+
+		JerseyClient jerseyClient = JerseyClientBuilder.createClient();
+		JerseyWebTarget jerseyTarget = jerseyClient.target(url);
+
+		MultivaluedMap<String, String> formData = new MultivaluedHashMap<String, String>();
+
+		Response response = jerseyTarget.request().header("Authorization","Bearer "+token).accept(MediaType.APPLICATION_JSON).post(Entity.form(formData));
+
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus()+ " "+ response.toString());
+		}
+		String output =  response.readEntity(String.class);
+		System.out.println(output);
+
+		return "Response : "+output;
 	}
 
 }
