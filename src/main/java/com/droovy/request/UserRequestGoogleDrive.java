9db67488726b7175ee25696269b52aae5b32ac59
valentin <valentin.lion@laposte.net>
2018-04-27 10:06:50 +0200

    Adapt Database

--- src/main/java/com/droovy/request/UserRequestGoogleDrive.java
+++ src/main/java/com/droovy/request/UserRequestGoogleDrive.java
@@ -1,10 +1,12 @@
 package com.droovy.request;
 
+import com.droovy.auth.GoogledriveAuth;
+
 public class UserRequestGoogleDrive implements UserRequest {
 
 	@Override
 	public String getFilesList() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
