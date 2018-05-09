41ed0d0dd8bc2fe4441548e212c77e86ceb8985b
Valentin Conseil <conseil.valentin96@gmail.com>
2018-05-09 20:59:28 +0200

    Ajout déplacement et renommage dropbox

--- src/main/java/com/droovy/request/UserRequestDropbox.java
+++ src/main/java/com/droovy/request/UserRequestDropbox.java
@@ -31,65 +31,69 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import errors.ApplicationException;
+import errors.UserFaultException;
+
 
 public class UserRequestDropbox implements UserRequest{
 
 	@Override
-	public List<File> getFilesList(String path,String id) {
+	public List<File> getFilesList(String path,String id) throws ApplicationException, UserFaultException {
 
 		String url = "https://api.dropboxapi.com/2/files/list_folder";
 		JSONParser parser = new JSONParserDropbox();
 
-
-		try{
-
-			if(path.equals("root")) {
-				path="";
-			}
-			
-			JerseyClient jerseyClient = JerseyClientBuilder.createClient();
-			JerseyWebTarget jerseyTarget = jerseyClient.target(url);
-
-			String jsonData = "{\"path\": \""+path+"\",\"recursive\": false,\"include_media_info\": false,\"include_deleted\": false,\"include_has_explicit_shared_members\": false,\"include_mounted_folders\": true }";
-
-			DatabaseOp db = new DatabaseOp();
-
-			Response response = jerseyTarget.request().header("Authorization", "Bearer "+db.getUserDropBoxToken(id)).header("Content-Type", "application/json").accept(MediaType.APPLICATION_JSON).post(Entity.json(jsonData));
-
-			if (response.getStatus() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : "
-						+ response.getStatus()+ " "+ response.toString());
-			}		
-
-			String output =  response.readEntity(String.class);
-
-			return parser.parserFiles(output);		
-
-		}catch(Exception e){
-			e.printStackTrace();
+		if(path.equals("root")) {
+			path="";
 		}
 
-		return new LinkedList<>();
+		JerseyClient jerseyClient = JerseyClientBuilder.createClient();
+		JerseyWebTarget jerseyTarget = jerseyClient.target(url);
 
+		String jsonData = "{\"path\": \""+path+"\",\"recursive\": false,\"include_media_info\": false,\"include_deleted\": false,\"include_has_explicit_shared_members\": false,\"include_mounted_folders\": true }";
+
+		DatabaseOp db = new DatabaseOp();
+
+		Response response = jerseyTarget.request().header("Authorization", "Bearer "+db.getUserDropBoxToken(id)).header("Content-Type", "application/json").accept(MediaType.APPLICATION_JSON).post(Entity.json(jsonData));
+
+		if (response.getStatus() != 200) {
+			if(response.getStatus()==401 || response.getStatus()==400) {
+				throw new UserFaultException("code",401);
+			}
+			else {
+				throw new ApplicationException();
+			}
+		}		
+
+		String output =  response.readEntity(String.class);
+
+		List<File> list = new LinkedList<>();
+		try {
+			parser.parserFiles(output);
+		} catch (Exception e) {
+			throw new ApplicationException();
+		}
+
+		return list;		
 	}
 
 
 	@Override
 	public boolean removeFile(String idFile,String path, String idUser) {
 		String url = "https://api.dropboxapi.com/2/files/delete_v2";
-		
+
 		try{
-			
+
 			JerseyClient jerseyClient = JerseyClientBuilder.createClient();
 			jerseyClient.register(MultiPartFeature.class);
 			JerseyWebTarget jerseyTarget = jerseyClient.target(url);
 
 			String jsonData = "{\"path\": \""+path+"\"}";
-			
+
 			DatabaseOp db = new DatabaseOp();
-			
-		    Response response = jerseyTarget.request().header("Content-Type", "application/json").header("Authorization", "Bearer "+db.getUserDropBoxToken(idUser)).accept(MediaType.APPLICATION_JSON).post(Entity.json(jsonData));
-			
+
+			Response response = jerseyTarget.request().header("Content-Type", "application/json").header("Authorization", "Bearer "+db.getUserDropBoxToken(idUser)).accept(MediaType.APPLICATION_JSON).post(Entity.json(jsonData));
+
 			if (response.getStatus() != 200) {
 				throw new RuntimeException("Failed : HTTP error code : "
 						+ response.getStatus()+ " "+ response.toString());
@@ -109,44 +113,44 @@ public class UserRequestDropbox implements UserRequest{
 		String url = "https://content.dropboxapi.com/2/files/upload";
 
 		System.out.println("upload ");
-		
+
 		try{
-			
+
 			JerseyClient jerseyClient = JerseyClientBuilder.createClient();
 			jerseyClient.register(MultiPartFeature.class);
 			JerseyWebTarget jerseyTarget = jerseyClient.target(url);
 
 			String jsonData = "{\"path\": \""+pathInDrive+"\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}";
-			
+
 			/*
 			 * --header "Authorization: Bearer " \
     --header "Dropbox-API-Arg: {\"path\": \"/Homework/math/Matrices.txt\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" \
     --header "Content-Type: application/octet-stream" \
     --data-binary @local_file.txt
 			 */
-			
+
 			DatabaseOp db = new DatabaseOp();
 
 			java.io.File file = new java.io.File(pathToFile);
-			
-			
+
+
 			byte[] fileData = new byte[(int) file.length()];
 			FileInputStream in = new FileInputStream(file);
 			in.read(fileData);
 			in.close();
 			System.out.println("file : "+fileData.toString());
-		
+
 			/*
 		    FileDataBodyPart filePart = new FileDataBodyPart("file", 
 		                                             new java.io.File(pathToFile));
-		    
+
 		    filePart.setContentDisposition(
 		            FormDataContentDisposition.name("file")
 		                                    .fileName(pathToFile).build());
 
-		 */
+			 */
 			/*
-			
+
 			 MultiPart multiPart = new MultiPart();
 		        multiPart.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
 
@@ -156,8 +160,8 @@ public class UserRequestDropbox implements UserRequest{
 		        multiPart.bodyPart(fileDataBodyPart);*/ 
 			System.out.println("uploading ");
 
-		    Response response = jerseyTarget.request().header("Dropbox-API-Arg", jsonData).header("Content-Type", "application/octet-stream").header("Authorization", "Bearer "+db.getUserDropBoxToken(userId)).post(Entity.entity(fileData.toString(), MediaType.APPLICATION_OCTET_STREAM));
-			
+			Response response = jerseyTarget.request().header("Dropbox-API-Arg", jsonData).header("Content-Type", "application/octet-stream").header("Authorization", "Bearer "+db.getUserDropBoxToken(userId)).post(Entity.entity(fileData.toString(), MediaType.APPLICATION_OCTET_STREAM));
+
 			System.out.println("uploaded ");
 
 			if (response.getStatus() != 200) {
@@ -176,15 +180,60 @@ public class UserRequestDropbox implements UserRequest{
 
 	@Override
 	public boolean renameFile(String idFile, String path, String name, String idUser) {
-		// TODO Auto-generated method stub
-		return false;
+		String url = "https://api.dropboxapi.com/2/files/move_v2";
+
+
+		JerseyClient jerseyClient = JerseyClientBuilder.createClient();
+		JerseyWebTarget jerseyTarget = jerseyClient.target(url);
+
+		String jsonData = "{\n" + 
+				"    \"from_path\": \""+path+"\",\n" + 
+				"    \"to_path\": \""+path.substring(0, path.lastIndexOf('/')+1)+name+"\",\n" + 
+				"    \"allow_shared_folder\": false,\n" + 
+				"    \"autorename\": false,\n" + 
+				"    \"allow_ownership_transfer\": false\n" + 
+				"}";
+		System.out.println("path : "+path.substring(0, path.lastIndexOf('/')+1)+name);
+		
+		DatabaseOp db = new DatabaseOp();
+
+		Response response = jerseyTarget.request().header("Authorization", "Bearer "+db.getUserDropBoxToken(idUser)).header("Content-Type", "application/json").accept(MediaType.APPLICATION_JSON).post(Entity.json(jsonData));
+
+		if (response.getStatus() != 200) {
+			if(response.getStatus() == 409) {
+				//doublon
+			}
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus()+ " "+ response.toString());
+		}		
+		return true;	
 	}
 
 
 	@Override
 	public boolean moveFile(String idFile, String path, String idParent, String pathParent, String idUser) {
-		// TODO Auto-generated method stub
-		return false;
+		String url = "https://api.dropboxapi.com/2/files/move_v2";
+
+
+		JerseyClient jerseyClient = JerseyClientBuilder.createClient();
+		JerseyWebTarget jerseyTarget = jerseyClient.target(url);
+
+		String jsonData = "{\n" + 
+				"    \"from_path\": \""+pathParent+"\",\n" + 
+				"    \"to_path\": \""+path+"\",\n" + 
+				"    \"allow_shared_folder\": false,\n" + 
+				"    \"autorename\": false,\n" + 
+				"    \"allow_ownership_transfer\": false\n" + 
+				"}";
+		DatabaseOp db = new DatabaseOp();
+
+		Response response = jerseyTarget.request().header("Authorization", "Bearer "+db.getUserDropBoxToken(idUser)).header("Content-Type", "application/json").accept(MediaType.APPLICATION_JSON).post(Entity.json(jsonData));
+
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatus()+ " "+ response.toString());
+		}		
+		return true;	
 	}
 
 
