fa12ce8ac6fce45eee06fb4feceb198fbdabb9c4
Matthlyc <37793706+Matthlyc@users.noreply.github.com>
2018-05-21 19:44:29 +0200

    rechargement de la page suite aux rename/delete

--- DroovyClient/src/app/file-modification/file-modification.component.ts
+++ DroovyClient/src/app/file-modification/file-modification.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {UploadDialog} from "../upload/upload.component";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-file-modification',
@@ -9,9 +10,21 @@ import {UploadDialog} from "../upload/upload.component";
 })
 export class FileModificationComponent implements OnInit {
 
-  constructor(private dialogRef: MatDialogRef<UploadDialog>,@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(private dialogRef: MatDialogRef<UploadDialog>,@Inject(MAT_DIALOG_DATA) public data: any, private router :Router) { }
 
   ngOnInit() {
   }
 
+  onRename(){
+    this.data.fileDisplay.rename();
+    this.router.navigate(['/manager']);
+    }
+  onDelete(){
+    this.data.fileDisplay.delete();
+    this.router.navigate(['/manager']);
+  }
+  onDownload(){
+    this.data.fileDisplay.downloas();
+  }
 }
+
